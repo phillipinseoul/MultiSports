8eb11f34532c2a1f4c7e01fb84ce935e33393c2d
Yuseung Lee <phillip9807@gmail.com>
2021-02-15 16:16:02 +0900

    add button & video

--- app/src/main/java/com/example/multisports/MainActivity.java
+++ app/src/main/java/com/example/multisports/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.multisports;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView1);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.messi_full);  // Set the path of the video "messi_full"
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);         // Link mediaController to videoView
+        videoView.setMediaController(mediaController);    // Allow mediaController to control our videoView
+        videoView.start();
     }
 }
